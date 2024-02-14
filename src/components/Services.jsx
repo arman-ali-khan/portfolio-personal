8c8f9225781a191beb185b3d60cf89d89d8fd8df
Arman Khan <sparmankhan@gmail.com>
2024-02-14 15:49:43 +0600

    update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,5 @@
-import { BiCodeCurly, BiSearchAlt } from "react-icons/bi";
-import { BsCode } from "react-icons/bs";
-import { GiPencilBrush } from "react-icons/gi";
-import { HiOutlineColorSwatch } from "react-icons/hi";
-import { TbMoneybag } from "react-icons/tb";
 import { Parallax } from "react-scroll-parallax";
 import useGetData from "../../lib/useGetData";
-import { IconContext } from "react-icons";
 const Services = () => {
   // get services data
   const {
@@ -18,56 +12,53 @@ const Services = () => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <IconContext.Provider value={{ size: "2em" }}>
-      <div className="de-container">
-        {/* Title */}
-        <div className="text-center my-6" id="services">
-          <Parallax scale={[1.2, 1, "easeInQuad"]}>
-            <h2 className="text-3xl font-bold">Services</h2>
-          </Parallax>
-          <p>What i offer</p>
-        </div>
-        {/* Body */}
-        <div>
-          {/* component */}
-          <div id="services" className="section relative  md:pb-0">
-            <div className="container xl:max-w-6xl mx-auto px-4">
-              {/* End heading */}
-              {/* row */}
-              <div className="de-service-wrap">
-                {servicesData.map((service) => {
-                  const IconComponent = eval(service?.icon);
-                  return (
-                    <div
-                      key={service.id}
-                      data-aos="slide-up"
-                      className="flex-shrink px- max-w-full w-full  wow "
-                      data-wow-duration="1s"
-                    >
-                      {/* service block */}
-                      <div className="de-service">
-                        <div className="inline-block mb-4">
-                          {/* icon */}
-                          <IconComponent />
-                        </div>
-                        {/* service name */}
-                        <h3 className="text-lg leading-normal mb-2 font-semibold ">
-                          {service.name}
-                        </h3>
-                        {/* service description */}
-                        <p className="text-gray-500">{service.description}</p>
+    <div className="de-container">
+      {/* Title */}
+      <div className="text-center my-6" id="services">
+        <Parallax scale={[1.2, 1, "easeInQuad"]}>
+          <h2 className="text-3xl font-bold">Services</h2>
+        </Parallax>
+        <p>What i offer</p>
+      </div>
+      {/* Body */}
+      <div>
+        {/* component */}
+        <div id="services" className="section relative  md:pb-0">
+          <div className="container xl:max-w-6xl mx-auto px-4">
+            {/* End heading */}
+            {/* row */}
+            <div className="de-service-wrap">
+              {servicesData.map((service) => {
+                return (
+                  <div
+                    key={service.id}
+                    data-aos="slide-up"
+                    className="flex-shrink px- max-w-full w-full  wow "
+                    data-wow-duration="1s"
+                  >
+                    {/* service block */}
+                    <div className="de-service">
+                      <div className="inline-block mb-4">
+                        {/* icon */}
+                        <img className="w-10" src={service?.icon} />
                       </div>
-                      {/* end service block */}
+                      {/* service name */}
+                      <h3 className="text-lg leading-normal mb-2 font-semibold ">
+                        {service.name}
+                      </h3>
+                      {/* service description */}
+                      <p className="text-gray-500">{service.description}</p>
                     </div>
-                  );
-                })}
-              </div>
-              {/* end row */}
+                    {/* end service block */}
+                  </div>
+                );
+              })}
             </div>
+            {/* end row */}
           </div>
         </div>
       </div>
-    </IconContext.Provider>
+    </div>
   );
 };
 
